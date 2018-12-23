204a0e36a308083973d5927498dd24303fae1231
Nozu <yusuke@YusukeYoshinobunoMacBook-Pro.local>
2018-12-23 20:33:26 +0900

    aaa

--- yoshinobu/write_csv.m
+++ yoshinobu/write_csv.m
@@ -1,13 +1,20 @@
 function write_csv(struct)
     %--------------------------------------------
-    %  struct : ファイル名、フォルダ名、評価、コメントが
-    %           含まれる
+    %  struct : ファイル名、フォルダ名、評価、コメントを
+    %           含む構造体
+    %    file_name : ファイル名
+    %         eval : 評価
+    %       coment : コメント
     %
     %     out_name : 出力ファイル名
     % out_file_type: 出力のファイル形式
     %--------------------------------------------
     out_file_type = '.csv';
-    folder_name = "output";
-    out_name = strcat(folder_name, '_evaluation', out_file_type);
-    dlmwrite(out_name, struct, '-append'); 
+    out_name = strcat( 'output_evaluation', out_file_type);
+    folder_name = struct.folder;
+    file_name = struct.name;
+    %eval;
+    %coment;
+    result = [folder_name, file_name];
+    dlmwrite(out_name, result, '-append'); 
 end
